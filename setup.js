80ab6034a436b11ac78362a07bbf53d3e8708cc1
Daniel Buchner <danieljb2@gmail.com>
2020-02-22 11:30:24 -0800

    finish setup changes

--- setup.js
+++ setup.js
@@ -3,13 +3,12 @@ const fs = require('fs-extra');
 
 function copyFiles(path){
   fs.copy('./spec-up', path).then(() => {
-    console.log('----- FILES COPIED -----');
+    console.log('--- Spec-Up files copied ---');
   }).catch(e => console.log(e));
 }
 
 let writeResources = async () => {
   try{
-    console.log(process.env.npm_lifecycle_event, process.env.npm_package_version);
     let event = process.env.npm_lifecycle_event;
     let config = await fs.readJson('../../specs.json');
     let resourcePath = `../../${
