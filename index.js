16ef4ddad28464814e410c39c8ca4f5b7a42dca4
csuwildcat <danieljb2@gmail.com>
2020-10-09 18:04:02 -0700

    async render

--- index.js
+++ index.js
@@ -141,87 +141,93 @@ module.exports = function(options = {}) {
         anchorClassName: 'toc-anchor'
       })
 
-    function render(spec, assets) {
+    async function render(spec, assets) {
       try {
         noticeTitles = {};
         specGroups = {};
         console.log('Rendering: ' + spec.title);
-        var features = (({ source, logo }) => ({ source, logo }))(spec);
-        var doc = (spec.markdown_paths || ['spec.md']).map(path => {
-          return fs.readFileSync(spec.spec_directory + path, 'utf8');
-        }).join("\n");
-        
-        fs.writeFileSync(spec.destination + 'index.html', `
-            <!DOCTYPE html>
-            <html lang="en">
-              <head>
-                <meta charset="utf-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
-                <title>${spec.title}</title>
+        return new Promise(async (resolve, reject) => {
+          Promise.all((spec.markdown_paths || ['spec.md']).map(path => {
+            return fs.readFile(spec.spec_directory + path, 'utf8').catch(e => reject(e))
+          })).then(async docs => {
+            var features = (({ source, logo }) => ({ source, logo }))(spec);
+            let doc = docs.join("\n");
+            fs.writeFile(spec.destination + 'index.html', `
+              <!DOCTYPE html>
+              <html lang="en">
+                <head>
+                  <meta charset="utf-8">
+                  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      
+                  <title>${spec.title}</title>
 
-                <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400&display=swap" rel="stylesheet">
+                  <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400&display=swap" rel="stylesheet">
 
-                ${assets.head}
-              </head>
-              <body features="${Object.keys(features).join(' ')}">
-                
-                ${assets.svg}
-    
-                <main>
-    
-                  <header id="header" class="panel-header">
-                    <span id="toc_toggle" panel-toggle="toc">
-                      <svg icon><use xlink:href="#svg-nested-list"></use></svg>
-                    </span>
-                    <a id="logo" href="${spec.logo_link ? spec.logo_link : '#_'}">
-                      <img src="${spec.logo}" />
-                    </a>
-                    <span issue-count animate panel-toggle="repo_issues">
-                      <svg icon><use xlink:href="#svg-github"></use></svg>
-                    </span>
-                  </header>
-    
-                  <article id="content">
-                    ${md.render(doc)}
-                  </article>    
-    
-                </main>
-    
-                <slide-panels id="slidepanels">
-                  <slide-panel id="repo_issues" options="right">
-                    <header class="panel-header">
-                      <span>
-                        <svg icon><use xlink:href="#svg-github"></use></svg>
-                        <span issue-count></span>
-                      </span>
-                      <span class="repo-issue-toggle" panel-toggle="repo_issues">✕</span>
-                    </header>
-                    <ul id="repo_issue_list"></ul>
-                  </slide-panel>
-    
-                  <slide-panel id="toc">
-                    <header class="panel-header">
-                      <span>Table of Contents</span>
-                      <span panel-toggle="toc">✕</span>
-                    </header>
-                    <div id="toc_list">
-                      ${toc}
-                    </div>
-                  </slide-panel>
+                  ${assets.head}
+                </head>
+                <body features="${Object.keys(features).join(' ')}">
                   
-                </slide-panels>
-    
-              </body>
-              <script>window.specConfig = ${JSON.stringify(spec)}</script>
-              ${assets.body}
-            </html>
-          `,
-          function(err, data){
-            if (err) console.log(err);
-          }
-        );
+                  ${assets.svg}
+      
+                  <main>
+      
+                    <header id="header" class="panel-header">
+                      <span id="toc_toggle" panel-toggle="toc">
+                        <svg icon><use xlink:href="#svg-nested-list"></use></svg>
+                      </span>
+                      <a id="logo" href="${spec.logo_link ? spec.logo_link : '#_'}">
+                        <img src="${spec.logo}" />
+                      </a>
+                      <span issue-count animate panel-toggle="repo_issues">
+                        <svg icon><use xlink:href="#svg-github"></use></svg>
+                      </span>
+                    </header>
+      
+                    <article id="content">
+                      ${md.render(doc)}
+                    </article>    
+      
+                  </main>
+      
+                  <slide-panels id="slidepanels">
+                    <slide-panel id="repo_issues" options="right">
+                      <header class="panel-header">
+                        <span>
+                          <svg icon><use xlink:href="#svg-github"></use></svg>
+                          <span issue-count></span>
+                        </span>
+                        <span class="repo-issue-toggle" panel-toggle="repo_issues">✕</span>
+                      </header>
+                      <ul id="repo_issue_list"></ul>
+                    </slide-panel>
+      
+                    <slide-panel id="toc">
+                      <header class="panel-header">
+                        <span>Table of Contents</span>
+                        <span panel-toggle="toc">✕</span>
+                      </header>
+                      <div id="toc_list">
+                        ${toc}
+                      </div>
+                    </slide-panel>
+                    
+                  </slide-panels>
+      
+                </body>
+                <script>window.specConfig = ${JSON.stringify(spec)}</script>
+                ${assets.body}
+              </html>
+            `, function(err, data){
+              if (err) {
+                reject(err);
+              }
+              else {
+                resolve();
+              }
+            }); 
+          });
+        });
       }
       catch(e) {
         console.log(e);
@@ -260,7 +266,9 @@ module.exports = function(options = {}) {
         )
       }
 
-      render.call(null, spec, assetTags);
+      render.call(null, spec, assetTags).then(() => {
+        if (options.nowatch) process.exit(0)
+      }).catch(() => process.exit(1));
 
     });
 
